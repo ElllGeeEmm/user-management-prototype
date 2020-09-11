8b8acd92eb37c3739642a096efb0ea8d979cc4d0
Robert Morrissey <rbrtmorrissey86@gmail.com>
2020-09-10 23:06:43 -0400

    mongoose skeleton

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,31 @@
-import { Controller, Get, Req, Post, Body } from '@nestjs/common';
+import { Controller, Get, Req, Post, Body, Patch, Param } from '@nestjs/common';
 import { Request } from 'express';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './users.dto';
 
-@Controller('/users')
+@Controller('users')
 export class UsersController {
   constructor(private userService: UsersService) {}
   @Get()
-  index (@Req() request: Request) {
-    return this.userService.findAll()
+  index(@Req() request: Request) {
+    return this.userService.read();
+  }
+
+  @Get(':id')
+  async view(@Param('id') userId: string) {
+    console.log(userId);
+    const user = await this.userService.readOne(userId);
+
+    return user;
   }
 
   @Post()
   create(@Body() user: CreateUserDto) {
-    return this.userService.create(user)
+    return this.userService.create(user);
+  }
+
+  @Patch(':id')
+  edit(@Param('id') userId: string, @Body() user: CreateUserDto) {
+    return this.userService.update(userId, user);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,15 +6,28 @@ import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
-  constructor(
-    @InjectModel(User.name) private userModel: Model<User>
-  ) {}
-  async findAll() {
-    return await this.userModel.find().exec()
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  async create(user: IUser) {
+    const newUser = new this.userModel(user);
+    return await newUser.save();
   }
 
-  async create(user: IUser) {
-    const newUser = new this.userModel(user)
-    return await newUser.save()
+  async read() {
+    return await this.userModel.find().exec();
+  }
+
+  async readOne(id: string) {
+    return await this.userModel.findById(id).exec();
+  }
+
+  async update(id: string, userUpdate: IUser) {
+    const user = await this.userModel.findById(id).exec();
+    user.set(userUpdate);
+    return await user.save();
+  }
+
+  async delete(id: string) {
+    const user = await this.userModel.findById(id).exec();
+    return await user.remove();
   }
 }
