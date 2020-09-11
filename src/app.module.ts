d1b6b2f280b7fa79bf79c3b6e956f6cf9e6879a5
Robert Morrissey <rbrtmorrissey86@gmail.com>
2020-09-10 21:20:20 -0400

    mongo init for dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { GroupsModule } from './groups/groups.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, MongooseModule.forRoot('mongodb://localhost/userManagement')],
 })
 export class AppModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import { User } from 'types/users/user.interface';
+import { IUser } from 'types/users/user.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from 'src/schemas/user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
-  private readonly users = [];
-  findAll() {
-    return this.users
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>
+  ) {}
+  async findAll() {
+    return await this.userModel.find().exec()
   }
 
-  create(user: User) {
-    this.users.push(user)
-    return user
+  async create(user: IUser) {
+    const newUser = new this.userModel(user)
+    return await newUser.save()
   }
 }
